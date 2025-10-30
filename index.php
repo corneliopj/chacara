7c3d6ebb5552696aca3756e22f81c1aadd3382d4
Cornélio Petersen Júnior <corneliopj@gmail.com>
2025-10-30 14:30:51 +0000

    Controle htaccess

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+// Define o caminho para o diretório public do Laravel
+define('LARAVEL_PUBLIC_PATH', __DIR__ . '/public');
+
+// Inclui o index.php do Laravel
+require LARAVEL_PUBLIC_PATH . '/index.php';
