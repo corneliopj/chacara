ce58d23ec63c6f5c166cb37dbb8ef23cb4d2e202
Cornélio Petersen Júnior <corneliopj@gmail.com>
2025-10-31 14:11:36 +0000

    web

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,40 @@
 <?php
 
+// routes/web.php
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\DespesaController;
-use App\Http\Controllers\ReceitaController;
 use App\Http\Controllers\CulturaController;
-use App\Http\Controllers\InventarioController;
-use App\Http\Controllers\TarefaController;
-use App\Http\Controllers\RelatoriosController;
+use App\Http\Controllers\DespesaController; // <-- ADICIONE ESTA LINHA
 
-// Rota Principal
-Route::redirect('/', '/dashboard');
+/*
+|--------------------------------------------------------------------------
+| Rotas do Aplicativo
+|--------------------------------------------------------------------------
+*/
 
-// Dashboard e API
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/api/graficos', [DashboardController::class, 'apiGraficos'])->name('api.graficos');
+// Rota principal (pode ser o Dashboard ou a listagem de culturas)
+Route::get('/', function () {
+    return redirect()->route('culturas.index');
+});
 
-// CRUDs
-Route::resource('culturas', CulturaController::class);
-Route::resource('despesas', DespesaController::class);
-Route::resource('receitas', ReceitaController::class);
-Route::resource('inventario', InventarioController::class);
-Route::resource('tarefas', TarefaController::class); // Tarefas: CRUD
+// Rotas RESTful para o CRUD de Culturas
+Route::resource('culturas', CulturaController::class)->names([
+    'index' => 'culturas.index',
+    'create' => 'culturas.create',
+    'store' => 'culturas.store',
+    'show' => 'culturas.show',
+    'edit' => 'culturas.edit',
+    'update' => 'culturas.update',
+    'destroy' => 'culturas.destroy',
+]);
 
-// Relatórios (POST para envio de filtros)
-Route::match(['get', 'post'], '/relatorios', [RelatoriosController::class, 'index'])->name('relatorios.index');
+// Rotas RESTful para o CRUD de Despesas
+Route::resource('despesas', DespesaController::class)->names([
+    'index' => 'despesas.index',
+    'create' => 'despesas.create',
+    'store' => 'despesas.store',
+    'show' => 'despesas.show',
+    'edit' => 'despesas.edit',
+    'update' => 'despesas.update',
+    'destroy' => 'despesas.destroy',
+]);
