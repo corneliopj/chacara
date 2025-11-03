9a55d727e8298948aca111f2632e9f5d206cf6d6
Cornélio Petersen Júnior <corneliopj@gmail.com>
2025-11-03 17:44:50 +0000

    vite

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+// resources/views/layout/master.blade.php (Apenas o <head>)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chácara Gestão | @yield('title', 'Dashboard')</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> 
+    
+    {{-- CRÍTICO: ATIVAR O VITE AQUI --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
     @stack('styles')
 </head>
 <body>
