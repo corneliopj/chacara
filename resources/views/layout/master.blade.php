175925648b856b95a76c86ef05926ae3265affcb
Cornélio Petersen Júnior <corneliopj@gmail.com>
2025-11-05 17:26:10 +0000

    @yield('scripts')

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -179,6 +179,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 
-@stack('scripts')
+@yield('scripts')
 </body>
 </html>
